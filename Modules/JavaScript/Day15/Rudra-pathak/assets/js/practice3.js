d3332a2f83df29e194993a0385ee765c877e57b0
Rudra-pathak <pathakrudra722@gmail.com>
2021-02-02 17:42:35 +0530

    commit

---
+++ Modules/JavaScript/Day15/Rudra-pathak/assets/js/practice3.js
@@ -0,0 +1,8 @@
+var cities = ["Ahmedabad", "Gandhinagar", "Rajkot", "Surat", "Mehsana"];
+var x = cities.length;
+var i;
+function displayCity() {
+    for (i = 0; i < x; i++) {
+        alert(cities[i]);
+    }
+}
