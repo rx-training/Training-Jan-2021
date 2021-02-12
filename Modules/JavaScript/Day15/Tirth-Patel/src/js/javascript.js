4e40a345ae9011cf04c6e530a4d6784072af424a
Admin <Admin@DESKTOP-QJNBM2I>
2021-02-12 16:49:50 +0530

    changes suggested is applied.

--- Modules/JavaScript/Day15/Tirth-Patel/src/js/javascript.js
+++ Modules/JavaScript/Day15/Tirth-Patel/src/js/javascript.js
@@ -11,10 +11,15 @@
 // console.log(vari)
 
 function greater() {
-    var max = 0;
+   
     var num1 = prompt("enter 1st number");
     var num2 = prompt("enter 2nd number");
     var num3 = prompt("enter 3rd number");
+    parseInt(num1);
+    parseInt(num2);
+    parseInt(num3);
+   
+   
     // if(num1 ==' ' || num3 ==' ' || num2 ==' ' ){
     //     alert("enter all 3 numbers");
 
@@ -37,8 +42,32 @@ function greater() {
         }
 
     }
+    if (num1 > 40) {
+        var maxn1 =parseInt( num1);
+    }
+    else{
+        maxn1=0;
+    }
+    if (num2 > 40) {
+        var maxn2 = parseInt(num2);
+    }
+    else{
+        maxn2=0;
+    }
+    if (num3 > 40) {
+        var maxn3 = parseInt(num3);
+    }
+    else{
+        maxn3=0;
+    }
+
+
+   var max40 = parseInt( maxn1 + maxn2 + maxn3);
+
+
 
     document.getElementById("max").innerHTML = "max number is " + max;
+    document.getElementById("max40").innerHTML = "sum of number value greater then 40 is " + max40;
 
 }
 function city() {
@@ -53,8 +82,8 @@ function calculate() {
     var result = 0;
     var numb1 = document.getElementById('number1').value;
     var numb2 = document.getElementById('number2').value;
-    numb1=parseInt(numb1);
-    numb2=parseInt(numb2);
+    numb1 = parseInt(numb1);
+    numb2 = parseInt(numb2);
     let opr = document.getElementsByName('op');
     let operator1;
 
@@ -82,7 +111,7 @@ function calculate() {
             console.log(result);
             break;
         default:
-        console.log('Enter valid input');
+            console.log('Enter valid input');
 
 
     }
@@ -90,8 +119,8 @@ function calculate() {
     document.getElementById('ans').innerHTML = result;
 
 }
- function reset(){
-     document.getElementById('add').checked =false;
-     alert('reset');
-     console.log('reset');
- }
+function reset() {
+    document.getElementById('add').checked = false;
+    alert('reset');
+    console.log('reset');
+}
