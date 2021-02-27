42582a7ae8bf96a6bfd0e69abf330357a2da7ca6
mastermind04 <65070588+mastermind04@users.noreply.github.com>
2021-02-27 12:34:34 +0530

    REFS: #81 and fixes #81 REDBUS admin

---
+++ Modules/Bootstrap/Day14/Smit vadhiya/Project Task/Project_redbus/src/js/buslist.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#userName").append(localStorage.getItem("currantUser"));
+    var foundData = JSON.parse(localStorage.getItem("found-buses"));
+    var allBus = JSON.parse(localStorage.getItem("buses"));
+    var route = JSON.parse(localStorage.getItem("search-buses"));
+    
+    var monthArray = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    var date = new Date(route.date);
+    var day = date.getDate();
+    var month = monthArray[date.getMonth()];
+    console.log(month,day);
+
+    var str1 = '<div class="d-flex flex-column flex-sm-row text-center justify-content-center"><div  class="h4 m-2">    <div > <span class="text-capitalize" >'+allBus[foundData[0]].start+'</span>  <i class=" h4 fa fa-long-arrow-right"></i> <span class="text-capitalize" > '+allBus[foundData[0]].end+'</span></div></div><div class="m-2 h4">    <div> &#10641; <span class="p-3">'+day+' '+month +' </span> &#10642; </div></div></div>'
+    $("#displayHeading").append(str1);
+
+
+    for(i of foundData){
+        var value = allBus[i];
+        
+        var bg = ""
+        if(value.rating < 3.0){
+            bg = "badge-danger";
+        } else if (value.rating < 4.0 ){
+            bg = "badge-warning";
+        } else if (value.rating >= 4.0 ){
+            bg = "badge-success";
+        }
+        
+        var str = '<div class="p-2 my-2 border border-dark"><div class="row justify-content-around"><div class="col-5 col-md-3"><h5> <i class="fa fa-bus"></i> '+value.busname+'</h5><p class="text-muted"> '+value.coach+'</p></div><div class="col-2 col-md-1 text-center"> <h5 >'+value.startTime+'</h5><p class="text-muted text-capitalize"> '+value.start+'</p></div><div class="col-3 col-md-2 text-center"> <p class="lead text-muted">2 hr 15 min</p></div><div class="col-2 col-md-1 text-center"> <h5>'+value.endTime+'</h5><p class="text-muted text-capitalize"> '+value.end+'</p></div><div class="col-4 col-md-1" > <div class="p-2 badge '+bg+'"><i class="fa fa-star"></i> '+value.rating+'</div><p class="text-muted"> <i class="fa fa-group"> 45</i></p></div><div class="col-4 col-md-2 "> <p class="text-muted d-inline"> INR</p><h4 class="d-inline" > '+value.farePrice+'</h4></div><div class="col-4 col-md-2"> <p class="text-muted d-inline"><b></b>45 Seats Available</p><h4 class="d-inline" ></h4></div></div><div class="d-flex flex-row-reverse"><div><button class="btn bg-theme" id="seat">Select seat</button></div></div></div>'  
+        $("#displayBus").append(str);
+    }
+    
+    $("#add").click(function(){
+       
+        
+    })
+})
+
