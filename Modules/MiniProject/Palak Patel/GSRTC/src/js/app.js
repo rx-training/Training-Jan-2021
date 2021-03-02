9128549450540398e7c940b467ae1d611d669f0e
Palak Patel <palakh.patel@rxteam376.onmicrosoft.com>
2021-03-03 00:05:10 +0530

    REFS: #82 and fixes #82 (admin panel complete)

---
+++ Modules/MiniProject/Palak Patel/GSRTC/src/js/app.js
@@ -0,0 +1,37 @@
+'use strict'
+var myStorage = window.localStorage;
+var admin = {
+    "username": "PalakPatel",
+    "password": "palak@1234"
+}
+myStorage.setItem('admin', JSON.stringify(admin));
+
+let flag = Number(JSON.parse(window.localStorage.getItem('flag')));
+document.getElementById('loginActivity').addEventListener('click', (e) => {
+        //alert('shgsw');
+        e.preventDefault();
+        if (flag == 1) {
+            window.location.href = 'admin.html';
+        } else {
+            window.location.href = 'login.html';
+        }
+    })
+
+document.getElementById('busRoute').addEventListener('click', function(e){
+    e.preventDefault();
+    let source = document.querySelector('.source').value;
+    let destination = document.querySelector('.dest').value;
+    let onward = document.querySelector('.onward').value;
+    let rtrn = document.querySelector('.return').value;
+
+    const userData = {
+        'source': source,
+        'destination' : destination,
+        'onward': onward,
+        'rtrn': rtrn
+    };
+
+    myStorage.setItem('userData', JSON.stringify(userData));
+
+    window.location.href = 'busRoute.html';
+});
---
+++ Modules/MiniProject/Palak Patel/GSRTC/src/js/busRoute.js
@@ -0,0 +1,93 @@
+
+let busInfo = JSON.parse(window.localStorage.getItem('buses'));
+let userData = JSON.parse(window.localStorage.getItem('userData'));
+let f = false;
+
+
+
+$.each(busInfo, function(i){
+    if(busInfo[i].Source.toLowerCase() == userData.source.toLowerCase() && busInfo[i].Destination.toLowerCase() == userData.destination.toLowerCase()){
+    $('#busLog').after(
+        `<div class="mt-5">
+        <div class="card d-block">
+            <div class="card-title">
+                <h6 class="pl-3 pt-3">Gurjarnagari Express</h6>
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="form-row align-items-center">
+                        <div class="col-lg-2">
+                            <h6 class="text-danger">${busInfo[i].BusNo}</h6>
+                        </div>
+                        <div class="col-lg-2">
+                            <table>
+                                <tr>
+                                    <td>Source</td>
+                                </tr>
+                                <tr>
+                                    <th>${busInfo[i].Source}</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-lg-2">
+                            <table>
+                                <tr>
+                                    <td>Destination</td>
+                                </tr>
+                                <tr>
+                                    <th>${busInfo[i].Destination}</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-lg-2">
+                            <table>
+                                <tr>
+                                    <td>Dept. Time</td>
+                                </tr>
+                                <tr>
+                                    <th>${busInfo[i].ArrivalTime}</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-lg-2">
+                            <table>
+                                <tr>
+                                    <td>Arrival Time</td>
+                                </tr>
+                                <tr>
+                                    <th>${busInfo[i].DepartureTime}</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-lg-1 d-flex justify-content-left">
+                            <table>
+                                <tr>
+                                    <td>Fare</td>
+                                </tr>
+                                <tr>
+                                    <th>${busInfo[i].Ticket}</th>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-lg-1 d-block">
+                            <button type="submit" class="btn btn-primary mb-2" id="seats">30</button>
+                            <span class="small text-primary">Select seats</span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>`
+    )}else{
+         f = true;    
+    }
+});
+
+if(f){
+    $('#busLog').after(
+        `<div class="mt-5">
+            <p>No Buses on this Route</p>
+        </div>`
+    )
+}
