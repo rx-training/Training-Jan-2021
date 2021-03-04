295be443277e71cc814f0b998db0e3e719e260c5
Admin <Admin@DESKTOP-QJNBM2I>
2021-03-04 12:28:30 +0530

    REFS: #120 and fixes #120

---
+++ Modules/SQL/Day1/Tirth-Patel/assignment.sql
@@ -0,0 +1,27 @@
+USE  CarSales
+
+CREATE TABLE SalesEmployees(
+	
+	EmpName VARCHAR(20),
+	EmpId VARCHAR(5) CONSTRAINT pkEmpId PRIMARY KEY,
+	EmpSalary money,
+	EmpCommission money	CONSTRAINT ukCommission UNIQUE,
+
+)
+
+CREATE TABLE CarInventories(
+	CarName VARCHAR(20),
+	CarId VARCHAR(5)  CONSTRAINT pkCarId PRIMARY KEY,
+	CarQuantity INT ,
+)
+
+CREATE TABLE CompleteSales
+(
+	EmpId varchar(5) CONSTRAINT fkEmpId FOREIGN KEY REFERENCES dbo.SalesEmployees(EmpId),
+	CarId Varchar(5) CONSTRAINT fkCarId FOREIGN KEY REFERENCES dbo.CarInventories(carId) ,
+	CarSalesId Varchar(5) CONSTRAINT pkCarSalesId PRIMARY KEY,
+)
+
+Select	 * FROM SalesEmployees
+SELECT * FROM CarInventories
+SELECT  * FROM CompleteSales
