1fbe12dfbd7f342efcb3718e68d9b03ca9dbc9c4
Fiyona Mistry <fiyonamistry2211@gmail.com>
2021-03-04 15:54:14 +0530

    REFS: #120 and fixes #120

--- Modules/SQL/Day1/FiyonaMistry/Assignment/assignment.sql
+++ Modules/SQL/Day1/FiyonaMistry/Assignment/assignment.sql
@@ -1,26 +1,31 @@
 USE Day1
 
-CREATE TABLE CarSales.Employees(
+
+CREATE TABLE CarSalesEmployees(
 	EmployeeId INT PRIMARY KEY,
 	EmployeeName VARCHAR(50) NOT NULL,
 	Email VARCHAR(255) NOT NULL,
+	PhoneNumber VARCHAR(50) NOT NULL,
 	JoiningDate DATE NOT NULL,
+	Salary MONEY NOT NULL,
 	Commission MONEY
 )
 
 
-CREATE TABLE CarSales.Inventories(
+CREATE TABLE CarSalesInventories(
 	CarId INT PRIMARY KEY,
 	CarName VARCHAR(50) NOT NULL,
 	CarModel VARCHAR(100) NOT NULL,
+	CarType VARCHAR(50) NOT NULL,
 	Price MONEY NOT NULL,
 	CarQuantity INT NOT NULL
 )
 
 
-CREATE TABLE CarSales.CompletedSales(
-	EmployeeId INT CONSTRAINT fkEmployeeId FOREIGN KEY REFERENCES CarSales.Employees(EmployeeId),
-	CardId INT CONSTRAINT fkCarId FOREIGN KEY REFERENCES CarSales.Inventories(CarId),
-	Price MONEY NOT NULL,
-	DateOfIssue DATE NOT NULL
+CREATE TABLE CarSalesCompletedSales(
+	SalesId INT PRIMARY KEY,
+	EmployeeId INT CONSTRAINT fkEmployeeId FOREIGN KEY REFERENCES CarSalesEmployees(EmployeeId),
+	CardId INT CONSTRAINT fkCarId FOREIGN KEY REFERENCES CarSalesInventories(CarId),
+	SalesCommission MONEY NOT NULL,
+	SalesDate DATE NOT NULL
 )
--- Modules/SQL/Day1/FiyonaMistry/Practice/practice.sql
+++ Modules/SQL/Day1/FiyonaMistry/Practice/practice.sql
@@ -2,7 +2,7 @@
 USE Day1
 
 
-CREATE TABLE dbo.Countries(
+CREATE TABLE Countries(
 	CountryId INT NOT NULL,
 	CountryName VARCHAR(50) CHECK (CountryName IN('Italy', 'India', 'China')),
 	RegionId INT NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE dbo.Countries(
 )
 
 
-CREATE TABLE dbo.JobHistories(
+CREATE TABLE JobHistories(
 	EmployeeId INT PRIMARY KEY,
 	StartDate DATE CHECK (StartDate LIKE('[0-3][0-9]/[0-1][0-9]/[0-3][0-9][0-9][0-9]')),
 	EndDate DATE CHECK (EndDate LIKE('[0-3][0-9]/[0-1][0-9]/[0-3][0-9][0-9][0-9]')),
@@ -19,7 +19,7 @@ CREATE TABLE dbo.JobHistories(
 )
 
 
-CREATE TABLE dbo.Jobs(
+CREATE TABLE Jobs(
 	JobId INT PRIMARY KEY,
 	JobTitle VARCHAR(50) DEFAULT '',
 	MinSalary MONEY DEFAULT '8000',
@@ -27,34 +27,34 @@ CREATE TABLE dbo.Jobs(
 )
 
 
-CREATE TABLE dbo.Departments(
+CREATE TABLE Departments(
 	DepartmentId INT PRIMARY KEY,
 	DepartmentName VARCHAR(50)
 )
 
 
-CREATE TABLE dbo.Employees(
+CREATE TABLE Employees(
 	EmployeeId INT PRIMARY KEY,
 	FirstName VARCHAR(50),
 	LastName VARCHAR(50),
 	Email VARCHAR(255),
 	PhoneNumber VARCHAR(50),
 	HireDate DATE,
-	JobId INT CONSTRAINT fkJobId FOREIGN KEY REFERENCES dbo.Jobs(JobId),
+	JobId INT CONSTRAINT fkJobId FOREIGN KEY REFERENCES Jobs(JobId),
 	Salary MONEY,
 	Commission INT,
 	ManagerId INT,
-	DepartmentId INT CONSTRAINT fkDepartmentId FOREIGN KEY REFERENCES dbo.Departments(DepartmentId)
+	DepartmentId INT CONSTRAINT fkDepartmentId FOREIGN KEY REFERENCES Departments(DepartmentId)
 )
 
 
-ALTER TABLE	dbo.JobHistories
-ADD CONSTRAINT fkJodId FOREIGN KEY (JobId) REFERENCES dbo.Jobs(JobId)
+ALTER TABLE	JobHistories
+ADD CONSTRAINT fkJodId FOREIGN KEY (JobId) REFERENCES Jobs(JobId)
 
 
-ALTER TABLE dbo.JobHistories
+ALTER TABLE JobHistories
 DROP CONSTRAINT fkJodId
 
 
-ALTER TABLE dbo.JobHistories
+ALTER TABLE JobHistories
 ADD Location VARCHAR(50)
