21196ac29c020e142f322ae4bd3bf84cb3e1448a
Chirag Fabiyani <Chirag Fabiyani@DESKTOP-J604RPS>
2021-03-05 15:26:57 +0530

    REFS: #121 and fixes #121 (Day2 SQL)

--- Modules/SQL/Day1/Chirag-Fabiyani/SQLDay1.sql
+++ Modules/SQL/Day1/Chirag-Fabiyani/SQLDay1.sql
@@ -9,6 +9,15 @@ CREATE TABLE Countries(
 	RegionId int,
 	CONSTRAINT UC_Person UNIQUE (CountryId,RegionId)
 );
+-----------------SAMPLE-CODE-----------------------
+CREATE TABLE CountriesSample(
+	CountryId int,
+	CountryName varchar(255) CHECK (CountryName IN ('Italy','India','China')),
+	RegionId int,
+	CONSTRAINT UC_Person UNIQUE (CountryId,RegionId)
+);
+
+
 
 
 -----------------PRACTICE-2------------------------
@@ -22,6 +31,10 @@ CREATE TABLE JobHistory(
 	Department_Id int
 );
 
+DROP TABLE JobHistory;
+
+
+
 
 -----------------PRACTICE-3------------------------
 
@@ -34,6 +47,10 @@ CREATE TABLE Jobs(
 );
 INSERT INTO Jobs(JobId) values(1);
 SELECT * FROM Jobs;
+
+
+
+
 
 
 -----------------PRACTICE-4------------------------
@@ -63,10 +80,16 @@ INSERT INTO Departments(DepartmentId) values (5003);
 INSERT INTO Employees ( Employee_Id, FirstName, LastName, Email, PhoneNumber, Hire_Date,Job_Id, Salary, Commission, Manager_Id, Department_Id) values (102 , 'kavush' , 'sharma', 'krs@mail.com', 757648 , '04-01-2021',501, 15000, 5, 405,5001);
 
 
+
+
+
 -----------------ALTER-1------------------------
 
 
 ALTER TABLE JobHistory ADD CONSTRAINT fk_job_id FOREIGN KEY (Job_Id) References Jobs1(JobId);
+
+
+
 
 
 -----------------ALTER-2------------------------
@@ -75,10 +98,16 @@ ALTER TABLE JobHistory ADD CONSTRAINT fk_job_id FOREIGN KEY (Job_Id) References 
 ALTER TABLE JobHistory DROP CONSTRAINT fk_job_id;
 
 
+
+
+
 -----------------ALTER-3------------------------
 
 
 ALTER TABLE JobHistory ADD Location varchar(255);
+
+
+
 
 
 -----------------ASSIGNMENT-1------------------------
@@ -97,6 +126,7 @@ CREATE TABLE Inventory(
 	AssignedSalesPersonId int CONSTRAINT FK_INVENTORY FOREIGN KEY (AssignedSalesPersonID) REFERENCES BusinessEmployees(EmployeeId),
 );
 CREATE TABLE CompletedSales(
+	TransactionNumber int CONSTRAINT PK_COMPLETEDSALES PRIMARY KEY(TransactionNumber),
 	EmployeeId int CONSTRAINT FK_COMPLETEDSALES FOREIGN KEY (EmployeeId) REFERENCES BusinessEmployees(EmployeeId),
 	PerMonthSales int,
 	MaxSoldProductId int CONSTRAINT FK_PRODUCT FOREIGN KEY (MaxSoldProductId) REFERENCES Inventory(VehicleId)
@@ -105,3 +135,4 @@ CREATE TABLE CompletedSales(
 SELECT * FROM BusinessEmployees
 SELECT * FROM Inventory
 SELECT * FROM CompletedSales
+
