92e6936f6338159b5a70d2240813655ed9b7aa7c
niraj125 <nirajsapra21@gmail.com>
2021-03-12 19:21:13 +0530

    REFS : #146 and Fixes :#146 (practice cluster and noncluster)

---
+++ Modules/SQL/Day8/Niraj-Sapra/assignment_SQL-MTA..sql
@@ -0,0 +1,90 @@
+
+USE AdventureWorks2012;
+
+CREATE TABLE HumanResources.Employees (EmployeeID decimal(6,0) NOT NULL DEFAULT '0',
+	FirstName varchar(20)  NULL,
+	LastName varchar(25) NOT NULL,
+	Email varchar(25) NOT NULL,
+	PhoneNumber varchar(20) DEFAULT NULL,
+	HireDate date NOT NULL,
+	JobId varchar(10) NOT NULL,
+	Salary decimal(6,0) DEFAULT NULL,
+	CommissionPct decimal(2,2) DEFAULT NULL,
+	ManagerID decimal(6,0) DEFAULT NULL,
+	DepartmentID decimal(4,0) DEFAULT NULL,
+);
+
+
+INSERT INTO HumanResources.Employees (EmployeeID,FirstName,LastName , Email, PhoneNumber, HireDate, JobId, Salary, CommissionPct, ManagerID, DepartmentID) VALUES
+('103', 'Steven', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES', '24000.00', '0.00', '0', '90'),
+('104', 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '1987-06-18', 'AD_VP', '17000.00', '0.00', '100', '90'),
+('102', 'Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '1987-06-19', 'AD_VP', '17000.00', '0.00', '100', '90'),
+('100', 'Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '1987-06-20', 'IT_PROG', '9000.00', '0.00', '102', '60'),
+('105', 'Bruce', 'Ernst', 'BERNST', '590.423.4568', '1987-06-21', 'IT_PROG', '6000.00', '0.00', '103', '60'),
+('101', 'David', 'Austin', 'DAUSTIN', '590.423.4569', '1987-06-22', 'IT_PROG', '4800.00', '0.00', '103', '60'),
+('109', 'Valli', 'Pataballa', 'VPATABAL', '590.423.4560', '1987-06-23', 'IT_PROG', '4800.00', '0.00', '103', '60'),
+('107', 'Diana', 'Lorentz', 'DLORENTZ', '590.423.5567', '1987-06-24', 'IT_PROG', '4200.00', '0.00', '103', '60'),
+('110', 'Nancy', 'Greenberg', 'NGREENBE', '515.124.4569', '1987-06-25', 'FI_MGR', '12000.00', '0.00', '101', '100'),
+('106', 'Daniel', 'Faviet', 'DFAVIET', '515.124.4169', '1987-06-26', 'FI_ACCOUNT', '9000.00', '0.00', '108', '100'),
+('118', 'John', 'Chen', 'JCHEN', '515.124.4269', '1987-06-27', 'FI_ACCOUNT', '8200.00', '0.00', '108', '100'),
+('111', 'Ismael', 'Sciarra', 'ISCIARRA', '515.124.4369', '1987-06-28', 'FI_ACCOUNT', '7700.00', '0.00', '108', '100'),
+('112', 'Jose Manuel', 'Urman', 'JMURMAN', '515.124.4469', '1987-06-29', 'FI_ACCOUNT', '7800.00', '0.00', '108', '100');
+
+
+/*Using UI get all clustor and nonculster both create this table*/ 
+
+/*---------------------------------------------------------
+Creating a Clustered Index Using Transact-SQL*/
+
+/* As a database administrator, you need to increase the performance of the PlanetsID table, so
+you decide to create a clustered index. But instead of using SSMS, you decide to use queries
+to perform this task. Therefore, you create a new PlanetsID database using the following
+commands within SMMS:*/
+
+
+USE AdventureWorks2012
+GO
+CREATE TABLE [dbo].[PlanetsID](
+[ID] [int] NOT NULL,
+[Item] [int] NOT NULL,
+[Value] [int] NOT NULL
+) ON [PRIMARY]
+GO
+INSERT INTO PlanetsID VALUES (4, 23, 66)
+INSERT INTO PlanetsID VALUES (1, 12, 59)
+INSERT INTO PlanetsID VALUES (3, 66, 24)
+SELECT * FROM PlanetsID
+GO
+
+
+/*CREATE CLUSTER*/
+
+CREATE CLUSTERED INDEX IX_PlanetsID_ID   
+    ON dbo.PlanetsID (ID);   
+GO  
+
+SELECT * FROM PlanetsID
+GO
+
+/*CREATE  NON-CLUSTER*/
+CREATE NONCLUSTERED INDEX IX_PlanetsID_Item   
+    ON dbo.PlanetsID (Item);   
+GO  
+SELECT * FROM PlanetsID
+GO
+
+
+/*
+-- Find an existing index named IX_ProductVendor_VendorID and delete it if found.   
+IF EXISTS (SELECT name FROM sys.indexes  
+            WHERE name = N'IX_ProductVendor_VendorID')   
+    DROP INDEX IX_ProductVendor_VendorID ON Purchasing.ProductVendor;   
+GO  
+-- Create a nonclustered index called IX_ProductVendor_VendorID   
+-- on the Purchasing.ProductVendor table using the BusinessEntityID column.   
+CREATE NONCLUSTERED INDEX IX_ProductVendor_VendorID   
+    ON Purchasing.ProductVendor (BusinessEntityID);   
+GO  
+SELECT * FROM PlanetsID
+GO */
+
