fb2893901affbd781fce046e8cc9a36f5602ad9d
ParthParekh7 <76997097+ParthParekh7@users.noreply.github.com>
2021-03-24 17:13:47 +0530

    REFS: #178 and fixes #178(Isolation level task done)

--- Modules/SQL/Day15/Parth Parekh/Day15_SQLAssignment.sql
+++ Modules/SQL/Day15/Parth Parekh/Day15_SQLAssignment.sql
@@ -18,3 +18,25 @@ AS
 GO
 EXEC TransferAmount  1,2,4000
 GO
+
+/*Problem Statement:
+At AdventureWorks, Inc., an employee named Sidney Higa, who is currently working as Production Technician – WC10 has been promoted as Marketing Manager. 
+The employee ID of Sidney is 13.
+As a database developer, you need to update his records. 
+This involves updating the title in the Employee table and updating the department history details.
+You need to ensure that all the changes take effect. 
+In addition, you need to ensure that no other transaction should be able to view the data being modified by the current transaction.*/
+
+SET TRANSACTION ISOLATION LEVEL
+READ COMMITTED
+BEGIN TRANSACTION TR
+BEGIN TRY
+	UPDATE HumanResources.Employee SET JobTitle = 'Marketing Manager' WHERE BusinessEntityID = 13
+	UPDATE HumanResources.EmployeeDepartmentHistory SET DepartmentID = 4 , ModifiedDate = GETDATE() WHERE  BusinessEntityID = 13		
+COMMIT TRANSACTION TR
+END TRY
+BEGIN CATCH 
+		ROLLBACK TRANSACTION TR 
+		SELECT 'Transaction Rollbacked' , ERROR_LINE() ,ERROR_MESSAGE()
+END CATCH
+
