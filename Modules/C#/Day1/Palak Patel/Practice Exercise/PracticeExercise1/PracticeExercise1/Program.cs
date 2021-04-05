be17c9b5515e1f0fda217833cffa60441a30c973
Palak Patel <palakh.patel@rxteam376.onmicrosoft.com>
2021-04-05 10:09:42 +0530

    REFS: #200 and fixes #200 (Task Completed)

---
+++ Modules/C#/Day1/Palak Patel/Practice Exercise/PracticeExercise1/PracticeExercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            var myString = "Hey It's my first time in C# !!";
+            Console.WriteLine(myString);
+            int first = 32;
+            double second = 45.62;
+            double sum = first + second;
+            Console.WriteLine(sum);
+
+            var items = new int[5]; //Declaration of integer type array
+
+            //Accepting Values From User
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine("Enter {0} Value", i+1);
+                //Storing Value in Array
+                items[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Printing in console
+            foreach (var item in items)
+            {
+                Console.WriteLine("{0}",item);
+            }
+
+            var sitems = items.sort
+        }
+    }
+}
