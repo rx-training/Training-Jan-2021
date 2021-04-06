936aebbfb7cce67c83d53a20cc66e6e2fc47552e
dpshah269 <dpshah269@gmail.com>
2021-04-06 11:24:18 +0530

    REFS: #200 and fixes #200(Did changes in C# Day2 Assignment)

--- Modules/C#/Day2/Disha-Shah/Assignment/AssignmentTask/Program.cs
+++ Modules/C#/Day2/Disha-Shah/Assignment/AssignmentTask/Program.cs
@@ -39,7 +39,7 @@ namespace AssignmentTask
         {
             get
             { 
-                if ((Convert.ToDateTime(DOB).Year - DateTime.Now.Year) > 18)
+                if ((DOB.Year - DateTime.Now.Year) > 18)
                 {
                     return "Adult";
                 }
@@ -54,47 +54,47 @@ namespace AssignmentTask
         {
             get
             {
-                if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("21-03") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("19-04"))
+                if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month) >= Convert.ToDateTime("21-03") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("19-04"))
                 {
                     return "Aries";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("20-04") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("20-05"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("20-04") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("20-05"))
                 {
                     return "Taurus";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("21-05") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("20-06"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("21-05") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("20-06"))
                 {
                     return "Gemini";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("21-06") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("22-07"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("21-06") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("22-07"))
                 {
                     return "Cancer";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("23-07") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("22-08"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("23-07") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("22-08"))
                 {
                     return "Leo";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("23-08") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("22-09"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("23-08") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("22-09"))
                 {
                     return "Virgo";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("23-09") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("22-10"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("23-09") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("22-10"))
                 {
                     return "Libra";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("23-10") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("21-11"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("23-10") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("21-11"))
                 {
                     return "Scorpio";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("22-11") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("21-12"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("22-11") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("21-12"))
                 {
                     return "Sagittarius";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("22-12") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("19-01"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("22-12") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("19-01"))
                 {
                     return "Capricorn";
                 }
-                else if (Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) >= Convert.ToDateTime("20-01") && Convert.ToDateTime((Convert.ToDateTime(DOB).Day + "-" + Convert.ToDateTime(DOB).Month)) <= Convert.ToDateTime("18-02"))
+                else if (Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) >= Convert.ToDateTime("20-01") && Convert.ToDateTime(DOB.Day + "-" + DOB.Month)) <= Convert.ToDateTime("18-02"))
                 {
                     return "Aquarius";
                 }
@@ -109,47 +109,47 @@ namespace AssignmentTask
         {
             get
             {
-                if ((Convert.ToDateTime(DOB).Year) == 1960 && (Convert.ToDateTime(DOB).Year) == 1972 && (Convert.ToDateTime(DOB).Year) == 1984 && (Convert.ToDateTime(DOB).Year) == 1996 && (Convert.ToDateTime(DOB).Year) == 2008 && (Convert.ToDateTime(DOB).Year) == 2032)
+                if (DOB % 12 == 4)
                 {
                     return "Rat";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1961 && (Convert.ToDateTime(DOB).Year) == 1973 && (Convert.ToDateTime(DOB).Year) == 1985 && (Convert.ToDateTime(DOB).Year) == 1997 && (Convert.ToDateTime(DOB).Year) == 2009 && (Convert.ToDateTime(DOB).Year) == 2033)
+                else if (DOB % 12 == 5)
                 {
                     return "Ox";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1962 && (Convert.ToDateTime(DOB).Year) == 1974 && (Convert.ToDateTime(DOB).Year) == 1986 && (Convert.ToDateTime(DOB).Year) == 1998 && (Convert.ToDateTime(DOB).Year) == 2010 && (Convert.ToDateTime(DOB).Year) == 2034)
+                else if (DOB % 12 == 6)
                 {
                     return "Tiger";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1963 && (Convert.ToDateTime(DOB).Year) == 1975 && (Convert.ToDateTime(DOB).Year) == 1987 && (Convert.ToDateTime(DOB).Year) == 1999 && (Convert.ToDateTime(DOB).Year) == 2011 && (Convert.ToDateTime(DOB).Year) == 2035)
+                else if (DOB % 12 == 7)
                 {
                     return "Rabbit";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1964 && (Convert.ToDateTime(DOB).Year) == 1976 && (Convert.ToDateTime(DOB).Year) == 1988 && (Convert.ToDateTime(DOB).Year) == 2000 && (Convert.ToDateTime(DOB).Year) == 2012 && (Convert.ToDateTime(DOB).Year) == 2036)
+                else if (DOB % 12 == 8)
                 {
                     return "Dragon";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1965 && (Convert.ToDateTime(DOB).Year) == 1977 && (Convert.ToDateTime(DOB).Year) == 1989 && (Convert.ToDateTime(DOB).Year) == 2001 && (Convert.ToDateTime(DOB).Year) == 2013 && (Convert.ToDateTime(DOB).Year) == 2037)
+                else if (DOB % 12 == 9)
                 {
                     return "Snake";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1966 && (Convert.ToDateTime(DOB).Year) == 1978 && (Convert.ToDateTime(DOB).Year) == 1990 && (Convert.ToDateTime(DOB).Year) == 2002 && (Convert.ToDateTime(DOB).Year) == 2014 && (Convert.ToDateTime(DOB).Year) == 2038)
+                else if (DOB % 12 == 10)
                 {
                     return "Horse";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1967 && (Convert.ToDateTime(DOB).Year) == 1979 && (Convert.ToDateTime(DOB).Year) == 1991 && (Convert.ToDateTime(DOB).Year) == 2003 && (Convert.ToDateTime(DOB).Year) == 2015 && (Convert.ToDateTime(DOB).Year) == 2039)
+                else if (DOB % 12 == 11)
                 {
                     return "Sheep";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1968 && (Convert.ToDateTime(DOB).Year) == 1980 && (Convert.ToDateTime(DOB).Year) == 1992 && (Convert.ToDateTime(DOB).Year) == 2004 && (Convert.ToDateTime(DOB).Year) == 2016 && (Convert.ToDateTime(DOB).Year) == 2040)
+                else if (DOB % 12 == 0)
                 {
                     return "Monkey";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1969 && (Convert.ToDateTime(DOB).Year) == 1981 && (Convert.ToDateTime(DOB).Year) == 1993 && (Convert.ToDateTime(DOB).Year) == 2004 && (Convert.ToDateTime(DOB).Year) == 2017 && (Convert.ToDateTime(DOB).Year) == 2041)
+                else if (DOB % 12 == 1)
                 {
                     return "Rooster";
                 }
-                else if ((Convert.ToDateTime(DOB).Year) == 1970 && (Convert.ToDateTime(DOB).Year) == 1982 && (Convert.ToDateTime(DOB).Year) == 1994 && (Convert.ToDateTime(DOB).Year) == 2005 && (Convert.ToDateTime(DOB).Year) == 2018 && (Convert.ToDateTime(DOB).Year) == 2042)
+                else if (DOB % 12 == 2)
                 {
                     return "Dog";
                 }
@@ -164,7 +164,7 @@ namespace AssignmentTask
         {
             get
             {
-                if (Convert.ToDateTime(DOB).Day == DateTime.Now.Day && Convert.ToDateTime(DOB).Month == DateTime.Now.Month)
+                if (DOB.Day == DateTime.Now.Day && DOB.Month == DateTime.Now.Month)
                 {
                     return "Happy Birthday!!";
                 }
@@ -178,7 +178,7 @@ namespace AssignmentTask
         {
             get
             {
-                string name = Firstname + Lastname + Convert.ToString(Convert.ToDateTime(DOB).Month) + Convert.ToString(Convert.ToDateTime(DOB).Day);
+                string name = Firstname + Lastname + Convert.ToString(DOB.Month) + Convert.ToString(DOB.Day);
                 return name;   
             }
         }
@@ -211,7 +211,7 @@ namespace AssignmentTask
                     Console.WriteLine("Enter DOB:");
                     dob = Convert.ToDateTime(Console.ReadLine());
                 }
-                while (Convert.ToDateTime(dob).Year > Convert.ToDateTime(DateTime.Now).Year && Convert.ToDateTime(dob).Month > Convert.ToDateTime(DateTime.Now).Month && Convert.ToDateTime(dob).Day > Convert.ToDateTime(DateTime.Now).Day);
+                while (dob.Year > DateTime.Now.Year && dob.Month > DateTime.Now.Month && dob.Day > DateTime.Now.Day);
 
                 person[i] = new Person(fname, lname, email, dob);
                 personinfo[i, 0] = person[i].Adult;
