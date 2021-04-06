afc395be43c4eec37a47df387734bb5e777fb5e3
tanna <tanna@LAPTOP-N1UDT3G5>
2021-04-06 19:33:37 +0530

    REFS :#200 and fixes #200(Completed Task)

--- Modules/C#/Day1/Charmi Tanna/Assignment/Assignment3/Assignment3/Program.cs
+++ Modules/C#/Day1/Charmi Tanna/Assignment/Assignment3/Assignment3/Program.cs
@@ -1,5 +1,14 @@
 ﻿using System;
-enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+enum Days
+{
+    Sunday = 1,
+    Monday = 2,
+    Tuesday = 3,
+    Wednesday = 4,
+    Thursday = 5,
+    Friday = 6,
+    Saturday = 7
+}
 namespace Assignment3
 {
     class Program
@@ -8,9 +17,32 @@ namespace Assignment3
         {
              static void Main(string[] args)
             {
-                string s = Enum.GetName(typeof(Days), 3);
-                Console.WriteLine(s);
-                Days d = Days.Sunday;
+                Console.WriteLine("Enter Week day:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine(Days.Sunday.ToString());
+                        break;
+                    case 2:
+                        Console.WriteLine(Days.Monday.ToString());
+                        break;
+                    case 3:
+                        Console.WriteLine(Days.Tuesday.ToString());
+                        break;
+                    case 4:
+                        Console.WriteLine(Days.Wednesday.ToString());
+                        break;
+                    case 5:
+                        Console.WriteLine(Days.Thursday.ToString());
+                        break;
+                    case 6:
+                        Console.WriteLine(Days.Friday.ToString());
+                        break;
+                    case 7:
+                        Console.WriteLine(Days.Saturday.ToString());
+                        break;
+                }
             }
         }
     }
