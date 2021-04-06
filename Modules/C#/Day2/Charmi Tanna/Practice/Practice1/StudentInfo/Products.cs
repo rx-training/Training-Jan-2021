0ea9c107d606d7bcc5ce23afeff4802e7b55d343
tanna <tanna@LAPTOP-N1UDT3G5>
2021-04-06 19:36:56 +0530

    REFS : #200 and fixes #200(Completed Task)

---
+++ Modules/C#/Day2/Charmi Tanna/Practice/Practice1/StudentInfo/Products.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentInfo
+{
+    //Constructors
+    public class Products
+    {
+        int id;
+        string name;
+        string category;
+        int price;
+        public Products()
+        {
+        }
+        public Products(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public Products(int id, string name,string category)
+        {
+            this.id = id;
+            this.name = name;
+            this.category = category;
+        }
+        public Products(int id, string name, string category, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.category = category;
+            this.price = price;
+        }
+        public void display()
+        {
+            Console.WriteLine($"ID is:{id},Name is:{name},category is: {category},Price is: {price}");
+        }
+    }
+}
