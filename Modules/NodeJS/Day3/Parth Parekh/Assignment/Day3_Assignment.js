cbc60be28c0c4a1eca805d9a9e3f75fbfa856479
ParthParekh7 <76997097+ParthParekh7@users.noreply.github.com>
2021-04-06 17:06:42 +0530

    REFS: #203 and fixes #203(Day3 NodeJs Task Done)

---
+++ Modules/NodeJS/Day3/Parth Parekh/Assignment/Day3_Assignment.js
@@ -0,0 +1,52 @@
+/*Rambo Rental Bikes is looking for developing a system to calculate the rentals of the bikes. 
+System should accept the customer details, bike details and calculate the rental charges.
+
+ DESCRIPTION OF PROJECTS
+
+System allows users to add customer details with bike rented. It computes rent for each customer.
+Systems displays the Bike details with summation of days of hire and rental payment.   FUNCTIONALITY AND TASK
+Define a class called Mobike with the following description: 
+Instance variables/data members: 
+BikeNumber – to store the bike’s number
+PhoneNumber – to store the phone number of the customer
+Name – to store the name of the customer
+Days – to store the number of days the bike is taken on rent o charge – to calculate and store the rental charge
+Member methods:
+void Compute( )
+– to compute the rental charge
+Bike No.         PhoneNo             No. of days         Charge
+ The rent for a mobike is charged on the following basis:
+First five days Rs 500 per day
+Next five days Rs 400 per day
+Rest of the days Rs 200 per day
+Note use get this Data @param ES6 class  Exports BikeNo,Phone,Noofdays and Charge.*/
+const chalk = require("chalk");
+class Mobike {
+    charges = 0;
+    constructor(BikeNumber, PhoneNumber, Name, Days) {
+        this.BikeNumber = BikeNumber;
+        this.PhoneNumber = PhoneNumber;
+        this.Name = Name;
+        this.Days = Days;
+    }
+
+    compute() {
+        if (this.Days >= 0 && this.Days <= 5) {
+            this.charges = this.Days * 500;
+        } else if (this.Days > 5 && this.Days <= 10) {
+            this.charges = 2500;
+            this.charges += (this.Days - 5) * 400;
+        } else if (this.Days > 10) {
+            this.charges = 4500;
+            this.charges += (this.Days - 10) * 200;
+        }
+    }
+    display() {
+        console.log(chalk.cyan(`Bike Number : ${this.BikeNumber}`));
+        console.log(chalk.cyan(`Name : ${this.Name}`));
+        console.log(chalk.cyan(`Phone Number : ${this.PhoneNumber}`));
+        console.log(chalk.cyan(`Number of Days : ${this.Days}`));
+        console.log(chalk.bgBlue(`Rent Charges : ${this.charges}`));
+    }
+}
+module.exports = Mobike;
---
+++ Modules/NodeJS/Day3/Parth Parekh/PracticeExercise/rectangle.js
@@ -0,0 +1,10 @@
+exports.areaofrectangle = (x, y) => {
+    console.log(`Area of Rectangle : ${x * y} `);
+};
+
+exports.perimeterofrectangle = (x, y) => {
+    console.log(`Perimeter of Rectangle : ${2 * (x + y)}`);
+};
+
+//Another Way
+//module.exports = {areaofrectangle,perimeterofrectangle};
