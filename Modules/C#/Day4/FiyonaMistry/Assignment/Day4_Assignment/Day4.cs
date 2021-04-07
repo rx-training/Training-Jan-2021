ef1972ef8e65d7af31f98f9542a9e2be3e854983
Fiyona Mistry <fiyonamistry2211@gmail.com>
2021-04-07 22:08:22 +0530

    REFS: #200 and fixes #200(Day 4)

---
+++ Modules/C#/Day4/FiyonaMistry/Assignment/Day4_Assignment/Day4.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Day4_Assignment
+{
+    class Day4
+    {
+        public class student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public DateTime Date { get; set; }
+
+            public void validateName()
+            {
+                for (int i = 0; i < Name.Length; i++)
+                {
+                    if(char.IsDigit(Name[i]) == false)
+                    {
+                        throw new NameException("Name should contain only characters");
+                    }
+                }
+            }
+
+            public void validateAge()
+            {
+                if(Age <= 0)
+                {
+                    throw new AgeException("Age should not be less than or equal to 0");
+                }
+            }
+
+            public void validateDate()
+            {
+                int result = DateTime.Compare(Date, System.DateTime.Now);
+                if(result < 0)
+                {
+                    throw new DateException("Date should not be less than current date");
+                }
+            }
+
+        }
+
+        public class AgeException : Exception 
+        {
+            public AgeException(string message) : base(message){ }
+        }
+
+        public class NameException : Exception
+        {
+            public NameException(string message) : base(message) { }
+        }
+
+        public class DateException : Exception
+        {
+            public DateException(string message) : base(message) { }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                student st = new student();
+
+                Console.Write("Enter Name : ");
+                st.Name = Console.ReadLine();
+
+                Console.Write("Enter Age : ");
+                st.Age = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Current Date : ");
+                st.Date = Convert.ToDateTime(Console.ReadLine());
+
+                st.validateName();
+                st.validateAge();
+                st.validateDate();
+            }
+            catch (NameException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (AgeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DateException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Modules/C#/Day4/FiyonaMistry/Practice/Day4_Practice/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Day4_Practice
+{
+    public class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            // This implementation handles a null obj argument. Usage error(logical error)
+            Person p = obj as Person;
+            if (p == null)
+                return false;
+            else
+                return this.Name.Equals(p.Name);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+                Person p1 = new Person();
+                p1.Name = "John";
+                Person p2 = null;
+
+                Console.WriteLine("p1 = p2: {0}", p1.Equals(p2));
+         
+        // The example displays the following output:
+        //        p1 = p2: False
+        }
+    }
+
+    // Exceptions : 
+    // Argument :  non-null argument that is passed to a method is invalid.
+    // ArgumentNull : An argument that is passed to a method is null.
+    // ArgumentOutOfRange
+    // DirectoryNotFound
+    // DivideByZero
+    // DriveNotFound
+    // FileNotFound
+    // Format
+    // IndexOutOfRange
+    // InvalidOperation : A method call is invalid in an object's current state.
+    // KeyNotFound
+    // NotImplemented
+    // NotSupported
+    // ObjectDisposed : An operation is performed on an object that has been disposed.
+    // Overflow
+    // PathTooLong
+    // PlatformNotSupported
+    // Rank : An array with the wrong number of dimensions is passed to a method.
+    // TimeOut
+    // UriFormat : An invalid Uniform Resource Identifier (URI) is used.
+
+    // Custom Exception Constructor : 
+    // Exception()
+    // Exception(String)
+    // Exception(String, Exception)
+    // Exception(SerializationInfo, StreamingContext)
+
+}
