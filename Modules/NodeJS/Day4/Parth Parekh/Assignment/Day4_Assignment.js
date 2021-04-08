39de1e1b23700d36a5dc1acd11cfc660006dd30a
ParthParekh7 <76997097+ParthParekh7@users.noreply.github.com>
2021-04-08 11:31:12 +0530

    REFS: #203 and fixes #203(Changes)

--- Modules/NodeJS/Day4/Parth Parekh/Assignment/Day4_Assignment.js
+++ Modules/NodeJS/Day4/Parth Parekh/Assignment/Day4_Assignment.js
@@ -10,10 +10,11 @@
 
 // http://localhost:3001/vowefind?input=rita
 
-// 4. Write a Nodejs server that serve as a RESTFUL API that accepts 
+// 4. Write a Nodejs server that serve as a RESTFUL API that accepts
 // a file content and writes them to the disk.
-
-http://localhost:3001/upload
+//http://localhost:3001/upload
+//for upload functionality. you need to keep one html file in your local system and 
+//then you need to read that file with the help of fs and send the response to the server in another folder
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
@@ -35,18 +36,17 @@ http.createServer((req, res) => {
     const myURL = new URL("http://localhost:3001" + req.url);
     console.log(myURL.pathname);
     if (myURL.pathname == "/product") {
-        const queryObject = url.parse(req.url, true).query;
-        var param1 = parseInt(queryObject.param1);
-        var param2 = parseInt(queryObject.param2);
-        var sum = param1 + param2;
-        res.write(`Sum of ${param1} and ${param2} is : ${sum}`);
+        let sum = 0;
+        let param1 = parseInt(myURL.searchParams.get("param1"));
+        let param2 = parseInt(myURL.searchParams.get("param2"));
+        sum = param1 + param2;
+        console.log(sum);
+        res.write(sum.toString());
         res.end();
     }
 
     if (myURL.pathname == "/vowefind") {
-        const queryObject = url.parse(req.url, true).query;
-        console.log(queryObject);
-        let input = queryObject.input;
+        let input = myURL.searchParams.get("input");
         let data = input.toLowerCase().split("");
         console.log(data);
         for (i = 1; i <= data.length; i++) {
@@ -73,9 +73,8 @@ http.createServer((req, res) => {
             console.log(data);
             res.write(data);
             res.end();
-        });    
+        });
     }
-
 }).listen(3001, () => {
     console.log("Running Server");
 });
