8d30aeb77b348798da22124724894906ee0e2392
Palak Patel <palakh.patel@rxteam376.onmicrosoft.com>
2021-04-08 12:27:30 +0530

    REFS: #200 and fixes #200 (Day4 Task Completed)

---
+++ Modules/C#/Day4/Palak Patel/Assignment/Assignment/Assignment/DateException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment
+{
+    class DateException : ApplicationException
+    {
+        public DateException()
+        {
+
+        }
+        public DateException(string Message):base(Message)
+        {
+
+        }
+
+        public void check (DateTime date)
+        {
+            if(date < DateTime.Today)
+            {
+                throw new DateException("Date should not be past to taoday's date. You cannot enter past date");
+            }
+        }
+    }
+}
---
+++ Modules/C#/Day4/Palak Patel/Assignment/Assignment/Assignment/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Assignment
+{
+    
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            AgeException a = new AgeException();
+            DateException d = new DateException();
+            NameException n = new NameException();
+
+            try
+            {
+                //checking age
+                Console.WriteLine("Enter Name:");
+                string name = Console.ReadLine();
+                n.check(name);
+
+                //checking age
+                Console.WriteLine("Enter Age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                a.check(age);
+
+                //checking Date
+                Console.WriteLine("Enter Date");
+                DateTime date = Convert.ToDateTime(Console.ReadLine());
+                d.check(date);
+            }
+            catch (AgeException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (DateException e)
+            { 
+                Console.WriteLine(e); 
+            }
+            catch(NameException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
