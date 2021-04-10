0dfb58c49f9d2ea51a33cec85f97d9a907775121
Fiyona Mistry <fiyonamistry2211@gmail.com>
2021-04-10 14:04:24 +0530

    REFS: #200 and fixes #200(Day 6)

---
+++ Modules/C#/Day6/FiyonaMistry/Assignment/Day6_Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day6_Assignment
+{
+    class Program
+    {
+        public delegate Area Func<in x, in y, out Area>(int x, int y);
+
+        public static int AreaMethod(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            // Compute area of rectangle using func delegate
+            int a, b;
+
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter width : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Func<int, int, int> area = AreaMethod;
+            Console.WriteLine("Area of rectangle : " + area(a, b));
+
+
+            // Compute add of two number using lambda expression
+            Func<int, int, int> Sum = (p, q) => p + q;
+            Console.WriteLine("Addition : " + Sum(a, b));
+
+        }
+    }
+}
---
+++ Modules/C#/Day6/FiyonaMistry/Practice/Day6_Practice/Clock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Day6_Practice
+{
+    public class Clock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public delegate void TimeChangeHandler(Object clock, TimeEventArgs timeInfo);
+        public event TimeChangeHandler TimeChanged;
+
+        public void RunClock()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                DateTime currentTime = DateTime.Now;
+                if(currentTime.Second != this.second)
+                {
+                    TimeEventArgs timeEventArgs = new TimeEventArgs()
+                    {
+                        Hour = currentTime.Hour,
+                        Minute = currentTime.Minute,
+                        Second = currentTime.Second
+                    };
+                    if(TimeChanged != null)
+                    {
+                        TimeChanged(this, timeEventArgs);
+                    }
+                    this.second = currentTime.Second;
+                    this.minute = currentTime.Minute;
+                    this.hour = currentTime.Hour;
+                }
+            }
+        }
+    }
+}
