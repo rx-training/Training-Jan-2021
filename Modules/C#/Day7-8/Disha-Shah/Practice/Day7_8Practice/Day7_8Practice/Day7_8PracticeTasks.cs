a4a95b552ed175b7e5c19fbc1e3cf917af47181c
dpshah269 <dpshah269@gmail.com>
2021-04-12 23:39:27 +0530

    REFS: #200 and fixes #200(Practiced LINQ from given site link)

---
+++ Modules/C#/Day7-8/Disha-Shah/Practice/Day7_8Practice/Day7_8Practice/Day7_8PracticeTasks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day7_8Practice
+{
+    class Day7_8PracticeTasks
+    {
+        static void Main(string[] args)
+        {
+            LINQPractice linqPractice = new LINQPractice();
+            linqPractice.Display();
+        }
+    }
+}
