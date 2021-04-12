d27479e91c5d4c6490871526736bd3a639fde1f9
namra22799 <76987423+namra22799@users.noreply.github.com>
2021-04-12 09:48:44 +0530

    REFS : #200 and fixes #200 (Day6Work)

---
+++ Modules/C#/Day6/Day6/Namra/Day6Assignment/Day6Assignment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Day6Assignment
+{
+    class Day6Assignment
+    {
+        delegate void Add(int num1, int num2);
+        static void Main(string[] args)
+        {
+            //Compute area of rectangle using func delegate
+
+            var AreaRectangle = new Func<int, int, string>((int length, int height) => {
+                if (length == 0 || height == 0)
+                {
+                    return $"Please enter valid length or height";
+                }
+
+                return $"Length and height of rectangle are {length}, {height} respectively and area is {length * height}";
+            });
+
+            Console.WriteLine($"Enter a length and a height of rectangle respectively");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(AreaRectangle(lenght, height));
+
+            //Compute add of two number using lambda expression
+
+            var AddNumbers = new Add((int num1, int num2) => {
+                Console.WriteLine($"Addition of two number {num1} and {num2} is {num2 + num1}");
+            });
+
+            Console.WriteLine($"-----------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine($"-----------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine($"Enter two number to perform addition of them");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            AddNumbers(num1, num2);
+        }
+    }
+}
---
+++ Modules/C#/Day6/Day6/Namra/Day6Practice/Day6Practice.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6Practice
+{
+    delegate void disp();
+    delegate void disp1(string msg);
+
+    class Test<t>
+    {
+        t number;
+        public Test( t num)
+        {
+            this.number = num;
+        }
+        public void displayTest()
+        {
+            Console.WriteLine($"Called [test] with number :{number}");
+        }
+    }
+    class demo1
+    {
+        public void display()
+        {
+            Console.WriteLine("Demo class is called");
+        }
+        public void display1(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class demoDate
+    {
+        public void getDate()
+        {
+            Console.WriteLine($"demo Date is called {DateTime.Now}");
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id : {Id} , Name : {Name}";
+        }
+    }
+    class eventClass
+    {
+        public event disp processComplete;
+        public void eventClassDemo()
+        {
+            Console.WriteLine("event is called");
+            processCompleted();
+        }
+        public void processCompleted()
+        {
+            processComplete?.Invoke();
+        }
+    }
+    class Day6Practice
+    {
+        public static void ec_processCompleted()
+        {
+            Console.WriteLine($"Event is finally called");
+        }
+        static void Main(string[] args)
+        {
+            demo1 d = new demo1();
+            disp demo = new disp(d.display);
+            demo += new disp(new demoDate().getDate);
+            //demo();
+            disp1 demo1 = new disp1(d.display1);
+            //demo1("Hello Namra here");
+
+            var p = new disp(delegate ()
+            {
+                Console.WriteLine("anonymous delegate");
+            });
+            //p();
+            var p1 = new disp(() => {
+                Console.WriteLine($"Lamba is called");
+            });
+            //p1();
+
+            var funcDelegate = new Func<string, string>((msg) => {
+                return msg;
+            });
+            //Console.WriteLine(funcDelegate("Hello func delegate here.."));
+
+            var funcDelegate2 = new Func<int, int, string>((a, b) => {
+                return $"Addition of {a} and {b} is {a + b}";
+            });
+            //Console.WriteLine(funcDelegate2(1, 2));
+
+            Person person = new Person() { Id = 1, Name = "N"};
+            Person person1 = new Person() { Id = 2, Name = "B" };
+            Dictionary<int, Person> dict = new Dictionary<int, Person>();
+            dict.Add(1, person);
+            dict.Add(2, person1);
+
+            Func<Person, string> funcPerson = new Func<Person, string>((obj) =>
+            {
+                return obj.ToString();
+            });
+
+            //foreach (var item in dict)
+            //{
+            //    Console.WriteLine(item.Value.ToString());
+            //} 
+            eventClass ec = new eventClass();
+            ec.processComplete += ec_processCompleted;
+            //ec.eventClassDemo();
+
+
+            int? number;
+            number = null;
+            if(number.HasValue || number == null)
+            {
+                Console.WriteLine($"value is null");
+            }
+            else
+            {
+                Console.WriteLine($"{number}");
+            }
+
+            Test<int> newTest = new Test<int>(12);
+            newTest.displayTest();
+        }
+    }
+}
