6de746d7a6aef64a49a1971edaf7175a38f87288
= <=>
2021-04-13 17:21:19 +0530

    Updated Training site with SQL , Detroit bank project

---
+++ Modules/Project/Milit-Panchasara/Training Site/src/mini_project_work/Detroit_Bank/src/js/admin/login.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var loginAdmin = function() { // loginAdmin class
+    // Properties
+    this.email = $('#email').val();
+    this.password = $('#password').val();
+    
+    
+};
+
+//function to login
+loginAdmin.prototype.login = async function() {
+    if(!this.validateFields(this.email, this.password)) {
+        return;
+    }
+
+    //do validations and redirect
+    var adminFile = await fetch("../json/admins.json");
+    var data = await adminFile.json();
+
+    //finds user and matches credentials
+    data.admins.forEach(admin => {
+        if(admin.email == this.email && admin.password == this.password) {
+            localStorage.setItem("logged_in_admin_data",JSON.stringify(admin));
+            localStorage.setItem("logged_in_admin",JSON.stringify(admin.id));
+            window.location.href = 'index.html';
+        }
+        else {
+            $('#login-error span').html('Invalid Credentials');
+            return;
+        }
+    });
+};
+
+loginAdmin.prototype.validateFields = function(email, password) {
+    let isValid = true;
+    $('#email-error span').html('');
+    $('#password-error span').html('');
+    if(email.trim() == "") {
+        $('#email-error span').html('Please enter E-mail');
+        isValid = false;
+    }
+    if(password.trim() == "") {
+        $('#password-error span').html('Please enter password');
+        isValid = false;
+    }
+
+    return isValid;
+}
