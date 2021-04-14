d5750f8dcace102bdc320909c0dcd796cd4b66e7
ParthParekh7 <76997097+ParthParekh7@users.noreply.github.com>
2021-04-14 18:48:11 +0530

    REFS: #203 and fixes #203(Day9 Task Done)

--- Modules/NodeJS/Day7/Parth Parekh/router1.js
+++ Modules/NodeJS/Day7/Parth Parekh/router1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const LoggerMiddleware = require("./loggerMiddleware");
 var router = express.Router();
 const empData = require("./employee.json");
-const fs = require('fs');
+const fs = require("fs");
 
 router.use(express.json());
 router.use(LoggerMiddleware);
@@ -51,7 +51,7 @@ router.put("/:id/child/assignments/:assignId", (req, res) => {
     const array = empData.find((c) => c.EmpId == req.params.id);
     let data = req.body;
     //console.log(data);
-    //Employee Exist or not 
+    //Employee Exist or not
     if (!array) {
         res.send(`EmployeeId not Found`);
         return;
@@ -65,16 +65,22 @@ router.put("/:id/child/assignments/:assignId", (req, res) => {
         if (!assignmentData) {
             res.send(`For  EmpId : ${req.params.id} AssignmentId Not Found`);
         } else {
-
-            for(let i = 0 ; i < empData.length ; i++)
-            {
-                for( let j = 0 ; j < empData[i].Assignments.length ; j++)
-                {
-                    //update Assignment Data
-                    empData[i].Assignments[j].AssignmentId = data.AssignmentId;
-                    empData[i].Assignments[j].AssignmentName = data.AssignmentName;
-                    empData[i].Assignments[j].AssignmentCategory = data.AssignmentCategory;
-                    empData[i].Assignments[j].AssignmentStatus = data.AssignmentStatus;
+            for (let i = 0; i < empData.length; i++) {
+                for (let j = 0; j < empData[i].Assignments.length; j++) {
+                    if (
+                        empData[i].Assignments[j].AssignmentId ==
+                        req.params.assignId
+                    ) {
+                        //update Assignment Data
+                        empData[i].Assignments[j].AssignmentId =
+                            data.AssignmentId;
+                        empData[i].Assignments[j].AssignmentName =
+                            data.AssignmentName;
+                        empData[i].Assignments[j].AssignmentCategory =
+                            data.AssignmentCategory;
+                        empData[i].Assignments[j].AssignmentStatus =
+                            data.AssignmentStatus;
+                    }
                 }
             }
             fs.writeFile("employee.json", JSON.stringify(empData), (err) => {
