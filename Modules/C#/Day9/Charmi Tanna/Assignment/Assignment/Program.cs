1f5dc29e0bac51be717b80ffd76685de37427f17
tanna <tanna@LAPTOP-N1UDT3G5>
2021-04-15 19:40:05 +0530

    REFS: #200 and fixes #200(Completed Task)

--- Modules/C#/Day9/Charmi Tanna/Assignment/Assignment/Program.cs
+++ Modules/C#/Day9/Charmi Tanna/Assignment/Assignment/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+/*Need to read the text file (content with comma like address) and write the same text into another
+* folder after delemiter 
+* with comma and after that delete that old file using async and await. using system.io namespace*/
 namespace Assignment
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string FilePath = "TextFile1.txt";
+            string file = "10, XYZ Appartment,Near XYZ Bunglows,Thaltej,Ahmedabad."+"\n"+
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad." + "\n" +
+            "10, XYZ Appartment, Near XYZ Bunglows, Thaltej, Ahmedabad.";
+            File.WriteAllText(FilePath, file);
+            string FilePath2 = "TextFile2.txt";
+            await Insert(FilePath,FilePath2);
+            
         }
-    }
+        public static async Task Insert(string FilePath, string FilePath2)
+        {
+            
+            string[] lines = File.ReadAllLines(FilePath);
+            foreach (string str in lines)
+            {
+                Console.WriteLine(str);
+            }
+            string s1 = null;
+            for (int j = 0; j < lines.Length; j++)
+            {
+                string[] str11 = lines[j].Split(',');
+
+                foreach (string s in str11)
+                {
+                    s1 = s1 + s + "\n";
+                }
+            }
+            File.WriteAllText(FilePath2, s1);
+            Console.WriteLine(s1);
+            await Delete(FilePath);
+        }
+            public static async Task Delete( string FilePath)
+            {
+                File.Delete(FilePath);
+            }
+            
+        }
 }
