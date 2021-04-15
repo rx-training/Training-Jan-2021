824cb5306dbec9c9c8934785d5292e6ce37124f8
tanna <tanna@LAPTOP-N1UDT3G5>
2021-04-15 17:10:57 +0530

    REFS: #200 and  fixes #200(Completed Task)

---
+++ Modules/C#/Day9/Charmi Tanna/Practice/Practice1/Practice1/StringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice1
+{
+    public static class StringHelper
+    {
+        public static string ChangeFirstLetterCase(this string input)
+        {
+            if(input.Length>0)
+            {
+                char[] array1 = input.ToCharArray();
+                array1[0] = char.IsUpper(array1[0]) ? char.ToLower(array1[0]) : char.ToUpper(array1[0]);
+                return new string(array1);
+            }
+            return input;
+        }
+    }
+}
---
+++ Modules/C#/Day9/Practice/Practice2/Practice2/StringUpper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice2
+{
+    public static class StringUpper
+    {
+        public static string ChangeStringUpper(this string str)
+        {
+            if(str!=null)
+            {
+                str = str.ToUpper();     
+            }
+            return str;
+        }
+    }
+}
