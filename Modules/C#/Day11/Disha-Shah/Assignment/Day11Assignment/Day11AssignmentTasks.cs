e6cf707d39c21f5385ea9c99bfe9f68c8b3becc3
dpshah269 <dpshah269@gmail.com>
2021-04-20 22:43:25 +0530

    REFS: #200 and fixes #200(Did minor changes in Day11 Assignment)

--- Modules/C#/Day11/Disha-Shah/Assignment/Day11Assignment/Day11AssignmentTasks.cs
+++ Modules/C#/Day11/Disha-Shah/Assignment/Day11Assignment/Day11AssignmentTasks.cs
@@ -1,8 +1,4 @@
-﻿using Day11Assignment.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System;
-using System.Collections.Generic;
+﻿using System;
 
 namespace Day11Assignment
 {
@@ -10,6 +6,9 @@ namespace Day11Assignment
     {
         static void Main(string[] args)
         {
+            DoctorInfo doctor = new DoctorInfo();
+            PatientInfo patient = new PatientInfo();
+
             bool moreChoice = true;
             while (moreChoice)
             {
@@ -21,26 +20,88 @@ namespace Day11Assignment
                 Console.WriteLine("5. Get Report of Medicines List for a particular Patient");
                 Console.WriteLine("6. Summary Report of Doctor and Patients");
                 Console.WriteLine("Enter your operation");
+
                 int op = Convert.ToInt32(Console.ReadLine());
                 switch (op)
                 {
                     case 1:
-                        InsertDoctor();
+                        // Enter Doctor Information
+                        Console.WriteLine("Enter Doctor Firstname: ");
+                        string fname = Console.ReadLine();
+                        Console.WriteLine("Enter Lastname:");
+                        string lname = Console.ReadLine();
+                        Console.WriteLine("Enter Address:");
+                        string address = Console.ReadLine();
+                        Console.WriteLine("Enter Contact No.:");
+                        int contact = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Age:");
+                        int age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Department name:");
+                        string dept = Console.ReadLine();
+
+                        doctor.InsertDoctor(fname, lname, address, contact, age, dept);
+
+                        Console.WriteLine("Data Added Successfully");
                         break;
                     case 2:
-                        UpdateDoctor();
+                        // Enter old name of doctor
+                        Console.WriteLine("Enter Firstname of Doctor whose information is to be modified:");
+                        string oldFname = Console.ReadLine();
+                        Console.WriteLine("Enter lastname of Doctor whose information is to be modified:");
+                        string oldLname = Console.ReadLine();
+
+                        // Enter new information of doctor
+                        Console.WriteLine("Enter Doctor Firstname: ");
+                        string newFname = Console.ReadLine();
+                        Console.WriteLine("Enter Lastname:");
+                        string newLname = Console.ReadLine();
+                        Console.WriteLine("Enter Address:");
+                        string newAddress = Console.ReadLine();
+                        Console.WriteLine("Enter Contact No.:");
+                        int newContact = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Age:");
+                        int newAge = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Department name:");
+                        string newDept = Console.ReadLine();
+
+                        doctor.UpdateDoctor(oldFname, oldLname, newFname, newLname, newAddress, newContact, newAge, newDept);
+
+                        Console.WriteLine("Data Updated Successfully");
                         break;
                     case 3:
-                        DeleteDoctor();
+                        // Enter name of doctor
+                        Console.WriteLine("Enter Firstname of Doctor whose information is to be deleted:");
+                        string oldFName = Console.ReadLine();
+                        Console.WriteLine("Enter lastname of Doctor whose information is to be deleted:");
+                        string oldLName = Console.ReadLine();
+
+                        doctor.DeleteDoctor(oldFName, oldLName);
+
+                        Console.WriteLine("Data deleted successfully");
                         break;
                     case 4:
-                        PatientDoctorReport();
+                        // Patients list under a particular doctor
+                        Console.WriteLine("Enter Name of Doctor whose information is to be searched:");
+                        string FName = Console.ReadLine();
+
+                        Console.WriteLine($"Information of Patients under {FName}:");
+                        Console.WriteLine();
+                        doctor.PatientDoctorReport(FName);
                         break;
                     case 5:
-                        PatientMedicineReport();
+                        // medicines list for a particular patient
+                        Console.WriteLine("Enter Name of Patient whose information is to be searched:");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine($"Medicines Information of {name}:");
+                        Console.WriteLine();
+                        patient.PatientMedicineReport(name);
                         break;
                     case 6:
-                        PatientDoctorSummary();
+                        // Patients information containing medicines list, doctors and personal information
+                        Console.WriteLine("Patients Details:");
+                        Console.WriteLine();
+                        patient.PatientDoctorSummary();
                         break;
                     default:
                         Console.WriteLine("Please choose correct option");
@@ -60,186 +121,6 @@ namespace Day11Assignment
 
             }
 
-        }
-
-        public static void InsertDoctor()
-        {
-            using (var context = new ERCore1DBContext())
-            {
-                Console.WriteLine("Enter Doctor Firstname: ");
-                string fname = Console.ReadLine();
-                Console.WriteLine("Enter Lastname:");
-                string lname = Console.ReadLine();
-                Console.WriteLine("Enter Address:");
-                string address = Console.ReadLine();
-                Console.WriteLine("Enter Contact No.:");
-                int contact = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Age:");
-                int age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Department name:");
-                string dept = Console.ReadLine();
-                var deptInfo = context.Departments
-                                    .Where(p => p.Name == dept)
-                                    .Select(p => new
-                                    {
-                                        deptId = p.Id
-                                    }).SingleOrDefault();
-
-                var departmentId = deptInfo.deptId;
-
-                var doctor = new Doctor() { FirstName = fname, Lastname = lname, Address=address, Contact=contact, Age=age, DepartmentId=departmentId};
-                context.Doctors.Add(doctor);
-                context.SaveChanges();
-
-                Console.WriteLine("Data Added Successfully");
-            }
-        }
-
-        public static void UpdateDoctor()
-        {
-            using (var context = new ERCore1DBContext())
-            {
-                Console.WriteLine("Enter Firstname of Doctor whose information is to be modified:");
-                string oldFname = Console.ReadLine();
-                Console.WriteLine("Enter lastname of Doctor whose information is to be modified:");
-                string oldLname = Console.ReadLine();
-                var updateDoctor = context.Doctors
-                                       .SingleOrDefault<Doctor>(x => x.FirstName == oldFname && x.Lastname==oldLname);
-
-                Console.WriteLine("Enter Doctor Firstname: ");
-                string fname = Console.ReadLine();
-                Console.WriteLine("Enter Lastname:");
-                string lname = Console.ReadLine();
-                Console.WriteLine("Enter Address:");
-                string address = Console.ReadLine();
-                Console.WriteLine("Enter Contact No.:");
-                int contact = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Age:");
-                int age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Department name:");
-                string dept = Console.ReadLine();
-                var deptInfo = context.Departments
-                                    .Where(p => p.Name == dept)
-                                    .Select(p => new
-                                    {
-                                        deptId = p.Id
-                                    }).SingleOrDefault();
-
-                var departmentId = deptInfo.deptId;
-
-                updateDoctor.FirstName = fname;
-                updateDoctor.Lastname = lname;
-                updateDoctor.Address = address;
-                updateDoctor.Contact = contact;
-                updateDoctor.Age = age;
-                updateDoctor.DepartmentId = departmentId;
-                context.SaveChanges();
-
-                Console.WriteLine("Data Updated Successfully");
-            }
-        }
-
-        public static void DeleteDoctor()
-        {
-            using (var context = new ERCore1DBContext())
-            {
-                Console.WriteLine("Enter Firstname of Doctor whose information is to be deleted:");
-                string oldFname = Console.ReadLine();
-                Console.WriteLine("Enter lastname of Doctor whose information is to be deleted:");
-                string oldLname = Console.ReadLine();
-
-                var deleteDoctor = context.Doctors
-                                           .SingleOrDefault<Doctor>(x => x.FirstName == oldFname && x.Lastname == oldLname);
-
-                context.Doctors.Remove(deleteDoctor);
-                context.SaveChanges();
-
-                Console.WriteLine("Data deleted successfully");
-            }
-        }
-
-        public static void PatientDoctorReport()
-        {
-            using (var context = new ERCore1DBContext())
-            {
-                Console.WriteLine("Enter Name of Doctor whose information is to be searched:");
-                string oldFname = Console.ReadLine();
-                
-                var report = context.VDoctorPatientReports
-                                    .Where(x=>x.Doctor==oldFname)
-                                    .ToList();
-
-                Console.WriteLine($"Information of Patients under {oldFname}:");
-                Console.WriteLine();
-                foreach (var item in report)
-                {
-                    Console.WriteLine($"Patient: {item.Patient}\nContact: {item.Contact}\nAge: {item.Age}\nAddress: {item.Address}\nProblem: {item.ProblemDesc}\nDepartment: {item.Department}\nHealthCare Taker: {item.HealthCarePerson}\nDoctor:{item.Doctor}");
-                    Console.WriteLine();
-                }
-            }
-        }
-
-        public static void PatientMedicineReport()
-        {
-            using (var context = new ERCore1DBContext())
-            {
-                Console.WriteLine("Enter Name of Patient whose information is to be searched:");
-                string oldFname = Console.ReadLine();
-
-                var report = context.VPatientMedicineReports
-                                    .Where(x => x.Patient == oldFname)
-                                    .ToList();
-
-                Console.WriteLine($"Medicines Information of {oldFname}:");
-                Console.WriteLine();
-                foreach (var item in report)
-                {
-                    Console.WriteLine($"Patient: {item.Patient}\nContact: {item.Contact}\nAge: {item.Age}\nProblem: {item.ProblemDesc}\nDepartment: {item.Department}\nMedicine: {item.Drugs}\nMedicine Description:{item.Description}\nExpiry date: {item.ExpiryDate}\nPrice: {item.Price}\nMedicineTime: {item.MedicineTime}");
-                    Console.WriteLine();
-                }
-            }
-        }
-
-        public static void PatientDoctorSummary()
-        {
-            using (var context = new ERCore1DBContext())
-            {
-                var summaryDoctorPatient = context.Patients
-                                            .Include(p => p.Department)
-                                            .Include(p => p.HealthCare)
-                                            .Include(p => p.PatientsDoctors)
-                                                .ThenInclude(g => g.Doctor)
-                                            .Include(p => p.PatientsDrugsShifts)
-                                                .ThenInclude(g => g.Drugs)
-                                                .ThenInclude(h => h.PatientsDrugsShifts)
-                                                    .ThenInclude(i => i.Shift)
-                                            .ToList();
-
-
-                Console.WriteLine("Patients Details:");
-                Console.WriteLine();
-                foreach (var item in summaryDoctorPatient)
-                {
-                    Console.WriteLine($"Patient name: {item.FirstName} {item.LastName}\nAge: {item.Age}\nContact No.: {item.Contact}\nProblem: {item.ProblemDesc}\nDepartment: {item.Department.Name}\nHealthCare Taker: {item.HealthCare.FirstName} {item.HealthCare.Lastname}");
-                    Console.Write("Doctors:");
-                    var doctorsList = new List<string>();
-
-                    foreach (var doctors in item.PatientsDoctors)
-                    {
-                        doctorsList.Add(doctors.Doctor.FirstName + " " + doctors.Doctor.Lastname);
-                    }
-                    Console.Write(String.Join(",", doctorsList.ToArray()));
-
-                    Console.WriteLine();
-                    Console.WriteLine("Medicines List:");
-                    foreach (var drugs in item.PatientsDrugsShifts)
-                    {
-                        Console.WriteLine($"{drugs.Drugs.Name}\t{drugs.Shift.Name}");
-                    }
-                    Console.WriteLine();
-
-                }
-            }
         }
     }
 }
