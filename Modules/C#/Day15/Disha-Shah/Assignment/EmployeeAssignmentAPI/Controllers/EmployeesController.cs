9ad964067e07a040b991dc63ea1dbd5a7aa989fb
dpshah269 <dpshah269@gmail.com>
2021-04-23 13:34:06 +0530

    REFS: #200 and fixes #200(Did minor changes in assignment)

--- Modules/C#/Day15/Disha-Shah/Assignment/EmployeeAssignmentAPI/Controllers/EmployeesController.cs
+++ Modules/C#/Day15/Disha-Shah/Assignment/EmployeeAssignmentAPI/Controllers/EmployeesController.cs
@@ -51,80 +51,6 @@ namespace EmployeeAssignmentAPI.Controllers
             return Ok(employee);
         }
 
-        // Get information of all assignments, assigned to particular employee
-        [HttpGet("{empId}/child/assignments")]
-        public ActionResult<List<Employee>> GetAllAssignments(long empId)
-        {
-            IList<Assignment> allAssignments = context.Assignments
-                                        .Include("Employee")
-                                       .Where(x => x.EmployeeId == empId)
-                                       .Select(s => new Assignment()
-                                       {
-                                           AssignmentId = s.AssignmentId,
-                                           AssignmentName = s.AssignmentName,
-                                           AssignmentStatus = s.AssignmentStatus,
-                                           StartDate = s.StartDate,
-                                           EndDate = s.EndDate,
-                                           EmployeeId = s.EmployeeId,
-                                           Employee = new Employee()
-                                           {
-                                               EmpId = s.Employee.EmpId,
-                                               FirstName = s.Employee.FirstName,
-                                               MiddleName = s.Employee.MiddleName,
-                                               LastName = s.Employee.LastName,
-                                               HireDate = s.Employee.HireDate,
-                                               Gender = s.Employee.Gender,
-                                               AddressLine1 = s.Employee.AddressLine1,
-                                               City = s.Employee.City
-                                           }
-                                       })
-                                    .ToList<Assignment>();
-
-            if (allAssignments.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(allAssignments);
-        }
-
-        // Get information of particular assignment, assigned to particular employee
-        [HttpGet("{empId}/child/assignments/{assignmentId}")]
-        public ActionResult<List<Employee>> GetAssignment(long empId, long assignmentId)
-        {
-            IList<Assignment> assignment = context.Assignments
-                                                    .Include("Employee")
-                                                   .Where(x => x.EmployeeId == empId && x.AssignmentId == assignmentId)
-                                                   .Select(s => new Assignment()
-                                                   {
-                                                       AssignmentId = s.AssignmentId,
-                                                       AssignmentName = s.AssignmentName,
-                                                       AssignmentStatus = s.AssignmentStatus,
-                                                       StartDate = s.StartDate,
-                                                       EndDate = s.EndDate,
-                                                       EmployeeId = s.EmployeeId,
-                                                       Employee = new Employee()
-                                                       {
-                                                           EmpId = s.Employee.EmpId,
-                                                           FirstName = s.Employee.FirstName,
-                                                           MiddleName = s.Employee.MiddleName,
-                                                           LastName = s.Employee.LastName,
-                                                           HireDate = s.Employee.HireDate,
-                                                           Gender = s.Employee.Gender,
-                                                           AddressLine1 = s.Employee.AddressLine1,
-                                                           City = s.Employee.City
-                                                       }
-                                                   })
-                                                .ToList<Assignment>();
-
-            if (assignment.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(assignment);
-        }
-
         // Add new employee
         [HttpPost]
         public ActionResult PostNewEmployee(Employee employee)
@@ -142,28 +68,6 @@ namespace EmployeeAssignmentAPI.Controllers
                 HireDate = employee.HireDate,
                 AddressLine1 = employee.AddressLine1,
                 City = employee.City
-            });
-
-            context.SaveChanges();
-
-            return Ok();
-        }
-
-        // Add new assignment
-        [HttpPost("{empId}/child/assignments")]
-        public ActionResult PostNewAssignment(Assignment assignment, long empId)
-        {
-
-            if (!ModelState.IsValid)
-                return BadRequest("Invalid data.");
-
-            context.Assignments.Add(new Assignment()
-            {
-                AssignmentName = assignment.AssignmentName,
-                AssignmentStatus = assignment.AssignmentStatus,
-                StartDate = assignment.StartDate,
-                EndDate = assignment.EndDate,
-                EmployeeId = empId
             });
 
             context.SaveChanges();
@@ -198,32 +102,6 @@ namespace EmployeeAssignmentAPI.Controllers
                 return NotFound();
             }
 
-            return Ok();
-        }
-
-        // Update assignment
-        [HttpPut("{empId}/child/assignments/{assignmentId}")]
-        public ActionResult PutAssignment(AssignmentDTO assignmentDTO, long empId, long assignmentId)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest("Not a valid data");
-           
-            var existingAssignment = context.Assignments.Where(s => s.AssignmentId == assignmentId && s.EmployeeId==empId)
-                                                    .SingleOrDefault<Assignment>();
-
-            if (existingAssignment != null)
-            {
-                existingAssignment.AssignmentStatus = assignmentDTO.AssignmentStatus;
-                existingAssignment.StartDate = assignmentDTO.StartDate;
-                existingAssignment.EndDate = assignmentDTO.EndDate;
-
-                context.SaveChanges();
-            }
-            else
-            {
-                return NotFound();
-            }
-            
             return Ok();
         }
     }
