7b00b9a43028733370537cbd99fa4a65b8057f0a
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-05-03 21:21:44 +0530

    AirIndia Database updated

--- Modules/Project/Ankur-Chovatiya/Node/WebApi/Project/controllers/B2C/Booking/select.js
+++ Modules/Project/Ankur-Chovatiya/Node/WebApi/Project/controllers/B2C/Booking/select.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const model = require('../../../MongoDB/model');
+const auth = require('../../../Middelware/jwt');
+const _ = require('lodash');
 
 
 class Select {
 
     static async selectFlight(req , res , next){
         
-
-        model.Flight.findOne(req.body).populate('AirFare').then(result =>{
+        var searchObj = _.pick(req.body ,['TakeoffPoint','LandingPoint', 'TackoffDate', 'LandingDate','Economy'])
+        model.Flight.findOne(searchObj).populate('AirFare').then(result =>{
             res.send(result);
             next();
         }).catch(err=>{
@@ -25,7 +27,7 @@ class Select {
 
 
 
-router.post('/' ,[express.json()] , Select.selectFlight);
+router.post('/' ,[express.json() , auth] , Select.selectFlight);
 
 
 module.exports = router;
