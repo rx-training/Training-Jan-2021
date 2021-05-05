e3b54403b4997d53441dc12fca1ae1028156dc88
tanna <tanna@LAPTOP-N1UDT3G5>
2021-05-05 10:41:02 +0530

    REFS : #245 and fixes #245(Completed Task)

---
+++ Modules/C#/Day15/Charmi Tanna/Assignment/Assignment1/Assignment1/Models/Assignments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Assignments
+    {
+        [Key]
+        public int AssignmentID { get; set; }
+        public string AssignmentName { get; set; }
+        public string AssignmentStatus { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime AssignmentProjectStartDate { get; set; }
+        public DateTime AssignmentProjectEndDate { get; set; }
+        public int EmployeeId { get; set; }
+        public virtual Employees Employees { get; set; }
+    }
+}
