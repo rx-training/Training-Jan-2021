da4a260d07830d0659b248517b7151681ef8f3fe
tanna <tanna@LAPTOP-N1UDT3G5>
2021-05-05 10:39:33 +0530

    REFS : #245 and fixes #245(Completed Task)

---
+++ Modules/C#/Day14/Charmi Tanna/Practice/Repository1/Repository1/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WEBAPI.Models
+{
+    public partial class Students
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int CourseId { get; set; }
+
+        public virtual Courses Course { get; set; }
+    }
+}
---
+++ Modules/C#/Day14/Charmi Tanna/Practice/Repository1/Repository1/Models/TestDBContext.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WEBAPI.Models
+{
+    public partial class TestDBContext : DbContext
+    {
+        public TestDBContext()
+        {
+        }
+
+        public TestDBContext(DbContextOptions<TestDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Adresses> Adresses { get; set; }
+        public virtual DbSet<Countries> Countries { get; set; }
+        public virtual DbSet<Course> Course { get; set; }
+        public virtual DbSet<Courses> Courses { get; set; }
+        public virtual DbSet<Departments> Departments { get; set; }
+        public virtual DbSet<Emphistory> Emphistory { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+        public virtual DbSet<JobHistory> JobHistory { get; set; }
+        public virtual DbSet<Locations> Locations { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<PersonSkills> PersonSkills { get; set; }
+        public virtual DbSet<Persons> Persons { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=NUCN181901\\MSSQL2017;Database=TestDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Adresses>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Area)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Flatno)
+                    .HasColumnName("flatno")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .HasColumnName("state")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Countries>(entity =>
+            {
+                entity.HasKey(e => e.CountryId)
+                    .HasName("PK__Countrie__10D160BFDC14CA0F");
+
+                entity.Property(e => e.CountryId)
+                    .HasColumnName("CountryID")
+                    .HasMaxLength(2)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CountryName)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RegionId)
+                    .HasColumnName("RegionID")
+                    .HasColumnType("decimal(10, 0)");
+            });
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.ToTable("course");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Discount)
+                    .HasColumnName("discount")
+                    .HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(90)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Courses>(entity =>
+            {
+                entity.HasKey(e => e.CourseId);
+            });
+
+            modelBuilder.Entity<Departments>(entity =>
+            {
+                entity.HasKey(e => e.DepartmentId)
+                    .HasName("PK__Departme__B2079BCDF2A8BD0C");
+
+                entity.Property(e => e.DepartmentId)
+                    .HasColumnName("DepartmentID")
+                    .HasColumnType("decimal(4, 0)")
+                    .HasDefaultValueSql("('0')");
+
+                entity.Property(e => e.DepartmentName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LocationId)
+                    .HasColumnName("LocationID")
+                    .HasColumnType("decimal(4, 0)");
+
+                entity.Property(e => e.ManagerId)
+                    .HasColumnName("ManagerID")
+                    .HasColumnType("decimal(6, 0)");
+            });
+
+            modelBuilder.Entity<Emphistory>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("emphistory");
+
+                entity.Property(e => e.EmployeeId)
+                    .HasColumnName("EmployeeID")
+                    .HasColumnType("decimal(6, 0)");
+
+                entity.Property(e => e.FirstnAme)
+                    .HasColumnName("FIRSTnAME")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LAstname)
+                    .IsRequired()
+                    .HasColumnName("lASTNAME")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("pkEmployeeID");
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("ukEmail")
+                    .IsUnique();
+
+                entity.Property(e => e.EmployeeId)
+                    .HasColumnName("EmployeeID")
+                    .HasColumnType("decimal(6, 0)")
+                    .HasDefaultValueSql("('0')");
+
+                entity.Property(e => e.CommissionPct).HasColumnType("decimal(2, 2)");
+
+                entity.Property(e => e.DepartmentId)
+                    .HasColumnName("DepartmentID")
+                    .HasColumnType("decimal(4, 0)");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.HireDate).HasColumnType("date");
+
+                entity.Property(e => e.JobId)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ManagerId)
+                    .HasColumnName("ManagerID")
+                    .HasColumnType("decimal(6, 0)");
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(8, 2)");
+            });
+
+            modelBuilder.Entity<JobHistory>(entity =>
+            {
+                entity.HasKey(e => new { e.EmployeeId, e.StartDate })
+                    .HasName("PK__JobHisto__1E31D03E21EF2CA5");
+
+                entity.Property(e => e.EmployeeId)
+                    .HasColumnName("EmployeeID")
+                    .HasColumnType("decimal(6, 0)");
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+
+                entity.Property(e => e.DepartmentId)
+                    .HasColumnName("DepartmentID")
+                    .HasColumnType("decimal(4, 0)");
+
+                entity.Property(e => e.EndDate).HasColumnType("date");
+
+                entity.Property(e => e.JobId)
+                    .IsRequired()
+                    .HasColumnName("JobID")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Locations>(entity =>
+            {
+                entity.HasKey(e => e.LocationId)
+                    .HasName("PK__Location__E7FEA4779135C8C6");
+
+                entity.Property(e => e.LocationId)
+                    .HasColumnName("LocationID")
+                    .HasColumnType("decimal(4, 0)")
+                    .HasDefaultValueSql("('0')");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CountryId)
+                    .HasColumnName("CountryID")
+                    .HasMaxLength(2)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PostalCode)
+                    .HasMaxLength(12)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StateProvince)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StreetAddress)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 0)");
+            });
+
+            modelBuilder.Entity<PersonSkills>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SkillId)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Persons>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AmountPayable).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Courseid).HasColumnName("courseid");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(90)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Student)
+                    .HasForeignKey(d => d.Courseid)
+                    .HasConstraintName("c1");
+            });
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.HasKey(e => e.StudentId);
+
+                entity.HasIndex(e => e.CourseId);
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Students)
+                    .HasForeignKey(d => d.CourseId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
