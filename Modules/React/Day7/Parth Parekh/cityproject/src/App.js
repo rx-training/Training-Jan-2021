c610541e578af23e5d83205d87cae7ad749cc205
ParthParekh7 <76997097+ParthParekh7@users.noreply.github.com>
2021-05-10 18:13:00 +0530

    REFS: #243 and fixes #243(Day7 Task Done)

---
+++ Modules/React/Day7/Parth Parekh/cityproject/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <TourList />
+        </>
+    );
+}
---
+++ Modules/React/Day7/Parth Parekh/cityproject/src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+    {
+        id: 1,
+        img: "./img/london.jpeg",
+        city: "London Tours",
+        name: "London",
+        info:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dolor!",
+    },
+    {
+        id: 2,
+        img: "./img/newyork.jpeg",
+        city: "New York Bridge Tours",
+        name: "New York",
+        info:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dolor!",
+    },
+    {
+        id: 3,
+        img: "./img/paris.jpeg",
+        city: "Paris Light Tours",
+        name: "Paris",
+        info:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dolor!",
+    },
+    {
+        id: 4,
+        img: "./img/tokyo.jpeg",
+        city: "Tokyo Sushi Tours",
+        name: "Tokyo",
+        info:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dolor!",
+    },
+];
