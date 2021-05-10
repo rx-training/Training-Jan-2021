092731ef539c7714985b6040d18181aab079672b
Nisu <nisargsoni2410@gmail.com>
2021-05-10 18:40:35 +0530

    REFS:#243 and fixes #243

---
+++ Modules/React/Day7/Nisarg/city-tour-project/src/tourData.js
@@ -0,0 +1,26 @@
+export const tourData=[
+{
+    id:1,
+    city:"new york",
+    img:"./img/newyork.jpg",
+    description:"Extra-ordinary city"
+},
+{
+    id:2,
+    city:"paris",
+    img:"./img/paris.jpg",
+    description:"Beautiful place"
+},
+{
+    id:3,
+    city:"London",
+    img:"./img/london.jpg",
+    description:"Exhilarating city"
+},
+{
+    id:4,
+    city:"Tokyo",
+    img:"./img/tokyo.jpg",
+    description:"No words to say"
+}
+]
