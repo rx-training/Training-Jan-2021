f1e1fa454c450b6eba2c52c449c0dba9705c5c82
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-05-12 22:06:56 +0530

    React Todo task done

--- Modules/React/Day8-9/Ankur_chovatiya/todo-list/src/components/todoItem.js
+++ Modules/React/Day8-9/Ankur_chovatiya/todo-list/src/components/todoItem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 export default class TodoItem extends Component {
+    
     render() {
+        const {title , handleDelete , handleEdit} = this.props;
         return (
-            <div>
-                <p>Todo items</p>
-            </div>
+            <li
+            className=" list-group-item text-capitalize d-flex justify-content-between my-2">
+               <h6>{title}</h6>
+               <div className="todo-icon">
+                    <span className="mx-2 text-success courser-pointer" onClick={handleEdit}>
+                        <i className="fas fa-pen"></i>
+                    </span>
+                    <span className="mx-2 text-denger" onClick={handleDelete}>
+                        <i className="fas fa-trash"></i>
+                    </span>
+               </div>
+            </li>
         )
     }
 }
--- Modules/React/Day8-9/Ankur_chovatiya/todo-list/src/components/todolist.js
+++ Modules/React/Day8-9/Ankur_chovatiya/todo-list/src/components/todolist.js
@@ -4,10 +4,31 @@ import TodoItem from './todoItem'
 
 export default class Todolist extends Component {
     render() {
+        const {items , clearList , handleDelete , handleEdit} = this.props;
         return (
             <div>
-               <h1>Todo list</h1> 
-               <TodoItem></TodoItem>
+                <ul className="list-group my-5">
+                    <h3 className="text-capitalize text-center">Todo list</h3>    
+                
+                {items.map((item)=> {
+                    return(
+                        <TodoItem key={item.id} title={item.title} 
+                        handleDelete={()=>handleDelete(item.id)}
+                        handleEdit={()=> handleEdit(item.id) }
+                        ></TodoItem>
+                    )
+                })}
+
+                <button
+                    type="button"
+                    className="btn btn-danger btn-block text-uppercase mt-5"
+                    onClick={clearList}
+                >
+                    Clear list
+                </button>
+
+                </ul>
+               
             </div>
         )
     }
