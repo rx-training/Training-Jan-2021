cb89e910d9bc82abfa72fc68f8515518f53ef634
dpshah269 <dpshah269@gmail.com>
2021-05-26 16:51:12 +0530

    REFS: #245 and fixes #245(Changed route path using lazy loading route configuration in Angular Day17/18 assignment)

--- Modules/Angular/Day17-18/Disha-Shah/Assignment/TrainingSiteApp/src/app/app-routing.module.ts
+++ Modules/Angular/Day17-18/Disha-Shah/Assignment/TrainingSiteApp/src/app/app-routing.module.ts
@@ -6,6 +6,17 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  {
+    path: 'html',
+    loadChildren: () => import('./html_work/html.module').then(m => m.HtmlModule)
+  },{
+    path: 'css',
+    loadChildren: () => import('./css_work/css.module').then(m => m.CssModule)
+  },
+  {
+    path: 'javascript',
+    loadChildren: () => import('./javascript_work/javascript.module').then(m => m.JavascriptModule)
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
 
