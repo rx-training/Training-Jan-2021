9218f1ab9341f1d118bfaaecf3bb9c04783ee651
Chirag Fabiyani <Chirag Fabiyani@DESKTOP-J604RPS>
2021-05-27 10:59:41 +0530

    REFS: #243 and fixes #243 (React Day16-18)

---
+++ Modules/React/Day18/Chirag-Fabiyani/cocktails-db/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">
+        about us
+      </h1>
+      <p>
+      I'm baby four dollar toast letterpress gentrify taxidermy pork belly cray heirloom bespoke waistcoat af. Hella marfa coloring book, kombucha chartreuse waistcoat bitters taiyaki keffiyeh 8-bit before they sold out pug actually enamel pin. Biodiesel selvage hexagon marfa meditation meh artisan schlitz, XOXO palo santo kombucha vinyl selfies cray yr. Blog salvia hell of jianbing unicorn, ethical tilde PBR&B truffaut.
+
+      Cornhole kickstarter disrupt kale chips humblebrag. Normcore authentic dreamcatcher williamsburg. Ethical leggings food truck, kickstarter tumeric skateboard cliche waistcoat succulents. Venmo next level swag fashion axe shoreditch, mumblecore skateboard. Crucifix everyday carry tbh actually cliche kinfolk biodiesel paleo cornhole food truck synth.
+      </p>
+    </section>
+  );
+}
