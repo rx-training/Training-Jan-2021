2c3668f85f3aec0bd702b5fc0e8d2ad3a1c737fd
ParthParekh7 <76997097+ParthParekh7@users.noreply.github.com>
2021-06-10 13:24:58 +0530

    REFS: #243 and fixes #243(minor change in otp showing)

--- Modules/Project/Parth Parekh/Node/Paytm/controllers/login/login.js
+++ Modules/Project/Parth Parekh/Node/Paytm/controllers/login/login.js
@@ -27,8 +27,22 @@ class LoginController {
                 from: USER,
                 to: email,
                 subject: "Verification",
-                html: `<h3 style = 'color : green'> OTP Verification :  ${otp} </h3> \n Please do not share 
-                this with anyone for security reasons `,
+                html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+      <div style="margin:50px auto;width:70%;padding:20px 0">
+        <div style="border-bottom:1px solid #eee">
+          <a href="" style="font-size:1.4em;color: rgb(1, 1, 139);text-decoration:none;font-weight:600">Paytm</a>
+        </div>
+        <p style="font-size:1.1em">Hi,</p>
+        <p>Thank you for choosing Paytm. Use the following OTP to complete your Sign Up procedures.</p>
+        <h2 style="background: rgb(1, 1, 139);margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+        <p style="font-size:0.9em;">Regards,<br />Paytm</p>
+        <hr style="border:none;border-top:1px solid #eee" />
+        <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+          <p>Paytm Inc</p>
+          <p>India</p>
+        </div>
+      </div>
+    </div>`,
             });
             res.status(200).send(`${otp}`);
         } catch (e) {
