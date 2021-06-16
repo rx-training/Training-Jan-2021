b94c81d3d1246327164c55b7adb2000f8d45e1ce
dpshah269 <dpshah269@gmail.com>
2021-06-16 22:38:37 +0530

    REFS: #245 and fixes #245(Practiced RxWeb Day1 plan)

---
+++ Modules/RxWeb/Day1/Practice/NewApplication/NewApplication/NewApplication.Models/ViewModels/StoreProcResult.cs
@@ -0,0 +1,11 @@
+
+namespace NewApplication.Models.ViewModels
+{
+    public class StoreProcResult
+    {
+        public int Id { get; set; }
+
+        public string Result { get; set; }
+    }
+}
+
---
+++ Modules/RxWeb/Day1/Practice/NewApplication/NewApplication/NewApplication.Models/ViewModels/UserConfig.cs
@@ -0,0 +1,11 @@
+
+namespace NewApplication.Models.ViewModels
+{
+    public class UserConfig
+    {
+
+        public string AudienceType { get; set; }
+
+        public string LanguageCode { get; set; }
+    }
+}
